0679eeb4052521001d3319335c2646e573643028
Artur Faria Vieira <artur.f.vieira.94@gmail.com>
2022-11-27 20:56:40 -0300

    arrumei sign in sign up

--- src/models/signinModel.js
+++ src/models/signinModel.js
@@ -2,7 +2,7 @@ import Joi from "joi"
 
 const userModel = Joi.object({
     email: Joi.string().email().required(),
-    senha: Joi.string().required()
+    password: Joi.string().required()
 });
 
 export default userModel;
